fdb290bd6c9d17fc32ccd52c8d11631b3d6b7fad
Christophe Lablancherie <clablancherie@cegep-ste-foy.qc.ca>
2018-12-22 10:22:51 -0500

    Ajout d'un slide dans la homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -144,6 +144,116 @@
                                         href="{{ route('page_about') }}">@lang("Voir plus")</a>
                             </div>
                         </li>
+                        <li data-index="rs21" data-transition="slidingoverlayhorizontal" data-slotamount="default"
+                            data-easein="default" data-easeout="default" data-masterspeed="default"
+                            data-thumb="{{ asset('images/slider-profs.jpg') }}" data-rotate="0" data-saveperformance="off"
+                            data-title="Slide 2" data-description="">
+                            <!-- MAIN IMAGE -->
+                            <img src="{{ asset('images/slider-profs.jpg') }}" alt="" data-bgposition="center 10%"
+                                 data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg"
+                                 data-bgparallax="10" data-no-retina>
+                            <!-- LAYERS -->
+
+                            <!-- LAYER NR. 1 -->
+                            <div class="tp-caption tp-resizeme text-uppercase text-white font-raleway"
+                                 id="rs-2-layer-1"
+
+                                 data-x="['left']"
+                                 data-hoffset="['30']"
+                                 data-y="['middle']"
+                                 data-voffset="['-110']"
+                                 data-fontsize="['100']"
+                                 data-lineheight="['110']"
+                                 data-width="none"
+                                 data-height="none"
+                                 data-whitespace="nowrap"
+                                 data-transform_idle="o:1;s:500"
+                                 data-transform_in="y:100;scaleX:1;scaleY:1;opacity:0;"
+                                 data-transform_out="x:left(R);s:1000;e:Power3.easeIn;s:1000;e:Power3.easeIn;"
+                                 data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
+                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
+                                 data-start="1000"
+                                 data-splitin="none"
+                                 data-splitout="none"
+                                 data-responsive_offset="on"
+                                 style="z-index: 7; white-space: nowrap; font-weight:700;">@lang('Les auteurs')
+                            </div>
+
+                            <!-- LAYER NR. 2 -->
+                            <div class="tp-caption tp-resizeme text-uppercase text-white font-raleway bg-theme-colored-transparent border-left-theme-color-2-6px pl-20 pr-20"
+                                 id="rs-2-layer-2"
+
+                                 data-x="['left']"
+                                 data-hoffset="['35']"
+                                 data-y="['middle']"
+                                 data-voffset="['-25']"
+                                 data-fontsize="['35']"
+                                 data-lineheight="['54']"
+                                 data-width="none"
+                                 data-height="none"
+                                 data-whitespace="nowrap"
+                                 data-transform_idle="o:1;s:500"
+                                 data-transform_in="y:100;scaleX:1;scaleY:1;opacity:0;"
+                                 data-transform_out="x:left(R);s:1000;e:Power3.easeIn;s:1000;e:Power3.easeIn;"
+                                 data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
+                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
+                                 data-start="1000"
+                                 data-splitin="none"
+                                 data-splitout="none"
+                                 data-responsive_offset="on"
+                                 style="z-index: 7; white-space: nowrap; font-weight:600;">@lang("Découvrez les auteurs de nos tutoriels")
+                            </div>
+
+                            <!-- LAYER NR. 3 -->
+                            <div class="tp-caption tp-resizeme text-white"
+                                 id="rs-2-layer-3"
+
+                                 data-x="['left']"
+                                 data-hoffset="['35']"
+                                 data-y="['middle']"
+                                 data-voffset="['35']"
+                                 data-fontsize="['16']"
+                                 data-lineheight="['28']"
+                                 data-width="none"
+                                 data-height="none"
+                                 data-whitespace="nowrap"
+                                 data-transform_idle="o:1;s:500"
+                                 data-transform_in="y:100;scaleX:1;scaleY:1;opacity:0;"
+                                 data-transform_out="x:left(R);s:1000;e:Power3.easeIn;s:1000;e:Power3.easeIn;"
+                                 data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
+                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
+                                 data-start="1400"
+                                 data-splitin="none"
+                                 data-splitout="none"
+                                 data-responsive_offset="on"
+                                 style="z-index: 5; white-space: nowrap; letter-spacing:0px; font-weight:400;">@lang("")
+                            </div>
+
+                            <!-- LAYER NR. 4 -->
+                            <div class="tp-caption tp-resizeme"
+                                 id="rs-2-layer-4"
+
+                                 data-x="['left']"
+                                 data-hoffset="['35']"
+                                 data-y="['middle']"
+                                 data-voffset="['100']"
+                                 data-width="none"
+                                 data-height="none"
+                                 data-whitespace="nowrap"
+                                 data-transform_idle="o:1;"
+                                 data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                                 data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                                 data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
+                                 data-start="1400"
+                                 data-splitin="none"
+                                 data-splitout="none"
+                                 data-responsive_offset="on"
+                                 style="z-index: 5; white-space: nowrap; letter-spacing:1px;"><a
+                                    class="btn btn-colored btn-lg btn-flat btn-theme-colored border-left-theme-color-2-6px pl-20 pr-20"
+                                    href="{{ route('teachers') }}">@lang("Voir plus")</a>
+                            </div>
+                        </li>
                     </ul>
                 </div>
                 <!-- end .rev_slider -->
